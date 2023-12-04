49e95fc50c44ae7ab0bc625dd504b96bba5b468a
sohajdaviktor <105918688+sohajdaviktor@users.noreply.github.com>
2023-12-04 21:42:38 +0100

    backup

---
+++ hajo/Controllers/TesztController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hajo.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return Ok(pontosIdő);
+        }
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            //nem mukszik
+            try
+            {
+                string nagybetusSzoveg = szoveg.ToUpper();
+                return Ok(szoveg.ToUpper());
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("Nem jó a bemenő adat!");
+            }
+            
+        }
+    }
+}
